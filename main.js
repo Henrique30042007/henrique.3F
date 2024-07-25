6eace99f24bdf77aee47b717ccc18284591c1742
Henrique30042007 <133656257+Henrique30042007@users.noreply.github.com>
2024-07-25 20:28:49 -0300

    Update main.js
    hhh

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclik = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
+        
         botoes[i].classlist.add("ativo");
+        textos[i].classlist.add("ativo");
     }
-    console.log(i);
+    
 }
