c2c13d3accbb553c1a50975c7ca32935021166aa
Henrique30042007 <133656257+Henrique30042007@users.noreply.github.com>
2024-08-01 22:26:28 -0300

    Update main.js
    hhh

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const botoes = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclik = function(){
+for(let i=0;i <botoes.length;i++) {
+    botoes[i].onclik = function() {
 
-        for(let j=0;j<botoes.length;j++){
+        for(let j=0;j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -14,3 +14,9 @@ for(let i=0;i <botoes.length;i++){
     }
     
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
